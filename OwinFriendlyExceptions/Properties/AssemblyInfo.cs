fb085a57d742b064d0d4a0c1e73b93f7230921cc
Anders Åberg <abergs.anders@gmail.com>
2015-03-06 16:53:46 +0100

    Fixed Assembly properties

--- OwinFriendlyExceptions/Properties/AssemblyInfo.cs
+++ OwinFriendlyExceptions/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OwinFriendlyExceptions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A way to translate Exceptions to friendly HTTP responses")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Anders Åberg")]
 [assembly: AssemblyProduct("OwinFriendlyExceptions")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
