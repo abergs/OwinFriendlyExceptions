2cfc9f131839d1cae0532a8c8e190f0d977973c8
Andriy S'omak <semack@hotmail.com>
2016-12-26 12:34:22 +0200

    Added MapOthers method

--- OwinFriendlyExceptions/ITransformsMap.cs
+++ OwinFriendlyExceptions/ITransformsMap.cs
@@ -6,6 +6,7 @@ namespace OwinFriendlyExceptions
     {
         ITransformTo<T> Map<T>() where T : Exception;
         ITransformTo<Exception> Map(Func<Exception, bool> matcher);
+        ITransformTo<Exception> MapOthers();
         ITransformsCollection Done();
     }
 }
